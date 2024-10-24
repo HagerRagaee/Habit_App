b3c20688108412ba87ba2307fd2b988bec6cc227
Rehab Nader <167696223+RehabNader003@users.noreply.github.com>
2024-10-24 18:30:13 +0300

    Update ForgotPassword.dart

--- lib/ForgotPassword.dart
+++ lib/ForgotPassword.dart
@@ -25,7 +25,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
             color: Colors.white,
           ),
         ),
-        backgroundColor: const Color.fromARGB(255, 163, 70, 240),
+        backgroundColor: const Color.fromARGB(255, 216, 138, 151),
         centerTitle: true,
       ),
       body: Stack(
@@ -36,12 +36,11 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
             height: double.infinity,
             decoration: const BoxDecoration(
               image: DecorationImage(
-                image: AssetImage("assets/images/cute.jpg"),
+                image: AssetImage("assets/images/background.jpg"),
                 fit: BoxFit.cover,
               ),
             ),
           ),
-          // محتوى النصوص والحقول
           Padding(
             padding: const EdgeInsets.all(20.0),
             child: Column(
@@ -57,7 +56,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                     color: Colors.white,
                     shadows: [
                       Shadow(
-                        color: Color.fromARGB(255, 163, 70, 240),
+                        color: Color.fromARGB(255, 201, 160, 220),
                         offset: Offset(2, 2),
                         blurRadius: 3.0,
                       ),
@@ -76,14 +75,14 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                       color: Colors.white,
                       shadows: [
                         Shadow(
-                          color: Color.fromARGB(255, 163, 70, 240),
+                          color: Color.fromARGB(255, 201, 160, 220),
                           offset: Offset(2, 2),
                           blurRadius: 3.0,
                         ),
                       ],
                     ),
                     prefixIcon: const Icon(Icons.email,
-                        color: Color.fromARGB(255, 163, 70, 240)),
+                        color: Color.fromARGB(255, 241, 194, 125)),
                     filled: true,
                     fillColor: Colors.white.withOpacity(0.7),
                     border: OutlineInputBorder(
@@ -92,7 +91,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                     focusedBorder: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(10),
                       borderSide: const BorderSide(
-                        color: Color.fromARGB(255, 163, 70, 240),
+                        color: Color.fromARGB(255, 211, 170, 232),
                       ),
                     ),
                   ),
@@ -137,7 +136,7 @@ class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
                       );
                     }
                   },
-                  color: const Color.fromARGB(255, 163, 70, 240),
+                  color: const Color.fromARGB(255, 216, 138, 151),
                   textColor: Colors.white,
                   padding:
                       const EdgeInsets.symmetric(vertical: 10, horizontal: 20),

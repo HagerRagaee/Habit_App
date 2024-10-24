368b4f999f86975490b3469c10c260c30a805521
Rehab Nader <167696223+RehabNader003@users.noreply.github.com>
2024-10-24 16:09:46 +0300

    Update today.dart

--- lib/today.dart
+++ lib/today.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'TaskItem.dart';
+import 'habitDialog.dart';
 
 class Today extends StatefulWidget {
   const Today({super.key});
@@ -27,12 +28,8 @@ class _TodayState extends State<Today> {
   getData() async {
     if (userId != null) {
       DateTime today = DateTime.now();
-      int todayWeekday =
-          today.weekday; // 1 for Monday, 2 for Tuesday, ..., 4 for Wednesday
-      int todayDay = today.day; // The day of the month
-
-      todayWeekday += 1;
-      print("Today is weekday: $todayWeekday"); // Debug print
+      int todayWeekday = today.weekday + 1; // 1 for Monday, 2 for Tuesday, etc.
+      int todayDay = today.day; // Day of the month
 
       // Fetch daily tasks and filter them by today's weekday
       QuerySnapshot dailySnapshot = await FirebaseFirestore.instance
@@ -42,11 +39,8 @@ class _TodayState extends State<Today> {
 
       dailyTasks = dailySnapshot.docs.where((task) {
         List<dynamic> repeatDays = task['repeat_days'] ?? [];
-        return repeatDays.contains(
-            todayWeekday); // Should correctly match with 4 for Wednesday
+        return repeatDays.contains(todayWeekday);
       }).toList();
-
-      print("Filtered daily tasks: ${dailyTasks.length}"); // Debug print
 
       // Fetch monthly tasks and filter them by today's day of the month
       QuerySnapshot monthlySnapshot = await FirebaseFirestore.instance
@@ -67,7 +61,6 @@ class _TodayState extends State<Today> {
     }
   }
 
-  // Function to fetch completed tasks for the current day
   Future<void> getCompletedTasks() async {
     if (userId != null) {
       try {
@@ -76,139 +69,49 @@ class _TodayState extends State<Today> {
             .where("user_id", isEqualTo: userId)
             .get();
 
-        // Check if the widget is still mounted before calling setState
         if (mounted) {
           setState(() {
             completedTasks = completedSnapshot.docs;
           });
         }
       } catch (e) {
-        // Handle any errors
         print("Error fetching completed tasks: $e");
-      }
-    }
-  }
-
-  Future<void> moveUncompletedTasks() async {
-    if (userId != null) {
-      try {
-        DateTime today = DateTime.now();
-        int todayWeekday = today.weekday;
-        todayWeekday += 1; // Get today's weekday
-
-        // Move uncompleted daily tasks
-        QuerySnapshot dailySnapshot = await FirebaseFirestore.instance
-            .collection("daily")
-            .where("user_id", isEqualTo: userId)
-            .get();
-
-        for (var task in dailySnapshot.docs) {
-          List<dynamic> repeatDays = task['repeat_days'] ?? [];
-          if (!task['completed'] && !repeatDays.contains(todayWeekday)) {
-            // Prepare task data for uncompleted collection
-            Map<String, dynamic> taskData = {
-              'task_name': task['task_name'],
-              'description': task['description'],
-              'color': task['color'],
-              'user_id': task['user_id'],
-              'created_at': FieldValue.serverTimestamp(),
-            };
-            // Add to uncompletedTasks collection
-            await FirebaseFirestore.instance
-                .collection("uncompletedTasks")
-                .add(taskData);
-            // Remove from daily collection
-            await FirebaseFirestore.instance
-                .collection("daily")
-                .doc(task.id)
-                .delete();
-          }
-        }
-
-        // Move uncompleted monthly tasks
-        QuerySnapshot monthlySnapshot = await FirebaseFirestore.instance
-            .collection("monthly")
-            .where("user_id", isEqualTo: userId)
-            .get();
-
-        for (var task in monthlySnapshot.docs) {
-          if (!task['completed']) {
-            // Prepare task data for uncompleted collection
-            Map<String, dynamic> taskData = {
-              'task_name': task['task_name'],
-              'description': task['description'],
-              'color': task['color'],
-              'user_id': task['user_id'],
-              'created_at': FieldValue.serverTimestamp(),
-            };
-            // Add to uncompletedTasks collection
-            await FirebaseFirestore.instance
-                .collection("uncompletedTasks")
-                .add(taskData);
-            // Remove from monthly collection
-            await FirebaseFirestore.instance
-                .collection("monthly")
-                .doc(task.id)
-                .delete();
-          }
-        }
-
-        print(
-            "Uncompleted tasks have been moved to uncompletedTasks collection.");
-      } catch (e) {
-        print("Error moving uncompleted tasks: $e");
       }
     }
   }
 
   void markTaskAsCompleted(DocumentSnapshot task, String collection) async {
     try {
-      // Step 1: Mark the task as completed by updating Firestore
       await FirebaseFirestore.instance
           .collection(collection)
           .doc(task.id)
           .update({'completed': true});
 
-      // Prepare task data for completed collection
       Map<String, dynamic> taskData = {
         'task_name': task['task_name'],
         'description': task['description'],
         'color': task['color'],
         'user_id': task['user_id'],
         'completed': true,
-        'completed_at':
-            FieldValue.serverTimestamp(), // Timestamp when completed
+        'completed_at': FieldValue.serverTimestamp(),
       };
 
-// Check if the task is from the daily or monthly collection
       if (collection == "daily") {
-        // Add repeat_days only for daily tasks
         taskData['repeat_days'] = task['repeat_days'];
       } else if (collection == "monthly") {
-        // Add repeat_dates only for monthly tasks
         taskData['repeat_dates'] = task['repeat_dates'];
       }
-      print(taskData);
-      // Step 2: Add the task to "completeTasks" collection
-      await FirebaseFirestore.instance
-          .collection("completeTasks")
-          .add(taskData);
 
-      // Step 3: Remove the task from the original collection
-      await FirebaseFirestore.instance
-          .collection(collection)
-          .doc(task.id)
-          .delete();
+      await FirebaseFirestore.instance.collection("completeTasks").add(taskData);
 
-      // Step 4: Update the local state
+      await FirebaseFirestore.instance.collection(collection).doc(task.id).delete();
+
       setState(() {
         if (collection == "daily") {
           dailyTasks.remove(task);
         } else if (collection == "monthly") {
           monthlyTasks.remove(task);
         }
-
-        // Add task to completedTasks list
         completedTasks.add(task);
       });
     } catch (e) {
@@ -221,7 +124,6 @@ class _TodayState extends State<Today> {
     return ListView(
       padding: const EdgeInsets.all(8.0),
       children: [
-        // Incomplete daily tasks section
         for (var task in dailyTasks)
           TaskItem(
             icon: Icons.track_changes,
@@ -236,11 +138,21 @@ class _TodayState extends State<Today> {
                 markTaskAsCompleted(task, "daily");
               },
             ),
+            onLongPress: () {
+              HabitOptionsDialog.showHabitOptionsDialog(
+                context,
+                task.id,
+                task['task_name'],
+                    () => HabitOptionsDialog().editHabit(context, task.id, "daily"), // Edit for daily habit
+                    () => HabitOptionsDialog().deleteHabit(context, task.id, "daily"), // Delete for daily habit
+                "daily",
+              );
+            },
+
           ),
 
         const SizedBox(height: 10),
 
-        // Incomplete monthly tasks section
         for (var task in monthlyTasks)
           TaskItem(
             icon: Icons.track_changes,
@@ -255,6 +167,17 @@ class _TodayState extends State<Today> {
                 markTaskAsCompleted(task, "monthly");
               },
             ),
+            onLongPress: () {
+              HabitOptionsDialog.showHabitOptionsDialog(
+                context,
+                task.id,
+                task['task_name'],
+                    () => HabitOptionsDialog().editHabit(context, task.id, "monthly"), // Edit for monthly habit
+                    () => HabitOptionsDialog().deleteHabit(context, task.id, "monthly"), // Delete for monthly habit
+                "monthly",
+              );
+            },
+
           ),
 
         const SizedBox(height: 10),
@@ -272,12 +195,21 @@ class _TodayState extends State<Today> {
         ),
         const SizedBox(height: 10),
 
-        // Completed tasks section
         for (var completedTask in completedTasks)
           TaskItem(
             icon: Icons.check_circle,
             TaskName: completedTask['task_name'],
-            TaskColor: Colors.green, // Completed task color
+            TaskColor: Colors.green,
+            onLongPress: () {
+              HabitOptionsDialog.showHabitOptionsDialog(
+                context,
+                completedTask.id,
+                completedTask['task_name'],
+                    () => HabitOptionsDialog().editHabit(context, completedTask.id, "completeTasks"), // Edit for completed task
+                    () => HabitOptionsDialog().deleteHabit(context, completedTask.id, "completeTasks"), // Delete for completed task
+                "completeTasks",
+              );
+            },
           ),
       ],
     );
